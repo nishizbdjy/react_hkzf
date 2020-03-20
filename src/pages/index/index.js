41abcc1bc3fb40b8917b92d5cefeb5623e50d7a6
lbw <2659154921@qq.com>
2020-03-20 23:55:37 +0800

    全局样式的设置和导航栏的渲染

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,46 +1,69 @@
 import React, { Component, Fragment } from 'react';
 //引入轮播图
 import { Carousel, WingBlank } from 'antd-mobile';
-// import axios from 'axios'
 //引入封装的axios
-import axios,{baseURL} from '../utils/request'
+import axios, { baseURL } from '../utils/request'
+//引入本地图片
+import img1 from '../../assets//images/nav-1.png'
+import img2 from '../../assets//images/nav-2.png'
+import img3 from '../../assets//images/nav-3.png'
+import img4 from '../../assets//images/nav-4.png'
 class Index extends React.Component {
   state = {
-    carouselList:[],
+    carouselList: [],
     imgHeight: 180,
+    //本地图片
+    bendiImg: [
+      { id: 0, text: "整租", imgSrc: img1 },
+      { id: 1, text: "合租", imgSrc: img2 },
+      { id: 2, text: "地图找房", imgSrc: img3 },
+      { id: 3, text: "去出租", imgSrc: img4 }
+    ]
   }
- async componentDidMount() {
- let res = await axios.get('/home/swiper')
-   this.setState({
-     carouselList: res.data.body
-   })
+  async componentDidMount() {
+    let res = await axios.get('/home/swiper')
+    this.setState({
+      carouselList: res.data.body
+    })
   }
   render() {
     return (
-      <WingBlank>
-        {this.state.carouselList.length&&<Carousel
-          autoplay
-          infinite
-        >
-          {this.state.carouselList.map(val => (
-            <a
-              key={val.id}
-              style={{ display: 'inline-block', width: '100%', height: this.state.imgHeight }}
-            >
-              <img
-                src={baseURL+val.imgSrc}
-                alt=""
-                style={{ width: '100%', verticalAlign: 'top' }}
-                onLoad={() => {
-                  // fire window resize event to change height
-                  window.dispatchEvent(new Event('resize'));
-                  this.setState({ imgHeight: 'auto' });
-                }}
-              />
-            </a>
-          ))}
-        </Carousel>}
-      </WingBlank>
+      <div>
+        {/*轮播图部分*/}
+        <WingBlank>
+          {this.state.carouselList.length && <Carousel
+            autoplay
+            infinite
+          >
+            {this.state.carouselList.map(val => (
+              <a
+                key={val.id}
+                style={{ display: 'inline-block', width: '100%', height: this.state.imgHeight }}
+              >
+                <img
+                  src={baseURL + val.imgSrc}
+                  alt=""
+                  style={{ width: '100%', verticalAlign: 'top' }}
+                  onLoad={() => {
+                    // fire window resize event to change height
+                    window.dispatchEvent(new Event('resize'));
+                    this.setState({ imgHeight: 'auto' });
+                  }}
+                />
+              </a>
+            ))}
+          </Carousel>}
+        </WingBlank>
+        {/* 列表导航*/}
+        <div className="index_nav">
+          {this.state.bendiImg.map(v => <div className="nav_item"
+            key={v.id}
+          >
+            <img src={v.imgSrc} alt="" />
+            <p>{v.text}</p>
+          </div>)}
+        </div>
+      </div>
     );
   }
 }

45b06d39dc9227918893dfcb862b246d9e4c66a5
lbw <2659154921@qq.com>
2020-03-21 11:19:44 +0800

    租房小组布局渲染

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -20,19 +20,35 @@ class Index extends React.Component {
       { id: 1, text: "合租", imgSrc: img2 },
       { id: 2, text: "地图找房", imgSrc: img3 },
       { id: 3, text: "去出租", imgSrc: img4 }
-    ]
+    ],
+    //租房小组数据
+    groups: []
   }
-  async componentDidMount() {
+  componentDidMount() {
+    //调用获取轮播图数据
+    this.carouselGet()
+    //调用获取租房小组数据
+    this.groupsGet()
+  }
+  //获取轮播图数据
+  carouselGet = async () => {
     let res = await axios.get('/home/swiper')
     this.setState({
       carouselList: res.data.body
+    })
+  }
+  //获取租房小组数据
+  groupsGet = async () => {
+    let res = await axios.get("/home/groups?area=AREA%7C88cff55c-aaa4-e2e0")
+    this.setState({
+      groups: res.data.body
     })
   }
   render() {
     return (
       <div>
         {/*轮播图部分*/}
-        <WingBlank style={{margin:0}}>
+        <WingBlank style={{ margin: 0 }}>
           {this.state.carouselList.length && <Carousel
             autoplay
             infinite
@@ -64,6 +80,26 @@ class Index extends React.Component {
             <img src={v.imgSrc} alt="" />
             <p>{v.text}</p>
           </div>)}
+        </div>
+        {/*租房小组 */}
+        <div className={NavCss.zufangxz}>
+          <div className={NavCss.zu_title}>
+            <h4>租房小组</h4>
+            <a href="#">更多</a>
+          </div>
+          <div className={NavCss.zu_content}>
+            {this.state.groups.map((v, i) => {
+              return <div className={NavCss.zf_item} key={i}>
+                <div className={NavCss.left}>
+                  <span>{v.title}</span>
+                  <span>{v.desc}</span>
+                </div>
+                <div className={NavCss.right}>
+                <img src={baseURL + v.imgSrc} alt="" />
+                </div>
+              </div>
+            })}
+          </div>
         </div>
       </div>
     );

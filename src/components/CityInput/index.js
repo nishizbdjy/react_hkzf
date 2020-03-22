93c896ea9d5fa343671913021a889bc8df47b7cf
lbw <2659154921@qq.com>
2020-03-22 18:25:41 +0800

    使用高性能函数优化输入框组件

--- src/components/CityInput/index.js
+++ src/components/CityInput/index.js
@@ -3,26 +3,23 @@ import React, { Component } from 'react';
 import InputCss from "./index.module.scss";
 //引入连接仓库的
 import { connect } from "react-redux";
-
-class CityInput extends Component {
-    render() {
-        return <div className={InputCss.zdy_input}>
-            <div className={InputCss.left}>
-                <div className={InputCss.City}>
-                    <span>{(this.props.CityGPS.name)}</span>
-                    <i className={["iconfont", "icon-arrow", InputCss.arrow].join(" ")}></i>
-                </div>
-                <div className={InputCss.input}>
-                    <i className={["iconfont", "icon-seach", InputCss.seach].join(" ")}></i>
-                    <span>请输入小区或地址</span>
-                </div>
-            </div>
-            <div className={InputCss.right}>
-                <i className={["iconfont", "icon-map", InputCss.map].join(" ")}></i>
-            </div>
+const  CityInput = React.memo( (props)=>{
+    return <div className={InputCss.zdy_input}>
+    <div className={InputCss.left}>
+        <div className={InputCss.City}>
+            <span>{(props.CityGPS.name)}</span>
+            <i className={["iconfont", "icon-arrow", InputCss.arrow].join(" ")}></i>
         </div>
-    }
-}
+        <div className={InputCss.input}>
+            <i className={["iconfont", "icon-seach", InputCss.seach].join(" ")}></i>
+            <span>请输入小区或地址</span>
+        </div>
+    </div>
+    <div className={InputCss.right}>
+        <i className={["iconfont", "icon-map", InputCss.map].join(" ")}></i>
+    </div>
+</div>
+})
 //重命名
 const chongmingming = (state) => {
     //state ：管理员仓库里面的defaultState

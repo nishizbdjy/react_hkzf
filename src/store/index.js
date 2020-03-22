8555fadb841663f4d7125f294c597cc578835723
lbw <2659154921@qq.com>
2020-03-22 19:03:47 +0800

    添加浏览器插件支持代码

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,20 @@
 //引入管理员
 import reducer from "./reducer";
 //中间件
-//创建仓库
-import { createStore,applyMiddleware  } from "redux";
+//创建仓库          compose//浏览器插件
+import { createStore,applyMiddleware,compose } from "redux";
 //异步
 import thunk from "redux-thunk";
+//添加浏览器插件支持代码
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+    }) : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(thunk),
+);
 //与管理员一起导出去
-export default createStore(reducer,applyMiddleware(thunk));
+const store = createStore(reducer, enhancer);
+export default  store;

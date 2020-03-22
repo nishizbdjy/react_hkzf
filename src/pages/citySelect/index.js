2d888528a4fa1dd2814651e92bebe6bdaf003d4a
lbw <2659154921@qq.com>
2020-03-22 18:55:04 +0800

    城市列表页添加导航栏

--- src/pages/citySelect/index.js
+++ src/pages/citySelect/index.js
@@ -1,8 +1,15 @@
 import React, { Component, Fragment } from 'react';
+import { NavBar, Icon } from 'antd-mobile';
 class citySelect extends Component {
   render() {
     return (
-      <Fragment>城市选择</Fragment>
+      <div>
+        <NavBar
+          mode="light"
+          icon={<Icon type="left" color="#505050" size="md" />}
+          onLeftClick={() =>this.props.history.go(-1)}
+        >城市选择</NavBar>
+      </div>
     );
   }
 }

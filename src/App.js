6ca45fdc23bb2f1fc5697b7c2bef04ec27a87978
lbw <2659154921@qq.com>
2020-03-23 13:58:44 +0800

    判断有城市数据再渲染组件(百度定位可以精准定位，不改为异步(跨域拦截))

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 //路由
 import { HashRouter as Router, Route, Link, Redirect } from "react-router-dom";
 //首页 ：tabBar页
@@ -11,32 +11,45 @@ import CitySelect from './pages/citySelect';
 import { connect } from "react-redux";
 //分拆action
 import { yibuaction } from "./store/fenchaiStore/index.js";
-function App(props) {
-  //调用分拆action获取城市
-  props.paifachaifen()
-  return (
-    <div>
-      <Router>
-        {/* 首页tabBar */}
-        <Route  path="/home" component={Home}></Route>
-        {/* 首页重定向 */}
-        <Route exact path="/">
-          <Redirect to="/home"></Redirect>
-        </Route>
-        {/* 地图找房 */}
-        <Route exact path="/mapFound" component={MapFound}></Route>
-        {/* 城市列表 */}
-        <Route exact path="/citySelect" component={CitySelect}></Route>
-      </Router>
-    </div>
-  );
+class App extends Component {
+  componentDidMount = () => [
+    //调用分拆action获取城市
+    this.props.paifachaifen()
+  ]
+  render() {
+    return (
+      <div>
+        {/* 有城市数据再渲染组件 */}
+        {this.props.CityGPS.name && <Router>
+          {/* 首页tabBar */}
+          <Route path="/home" component={Home}></Route>
+          {/* 首页重定向 */}
+          <Route exact path="/">
+            <Redirect to="/home"></Redirect>
+          </Route>
+          {/* 地图找房 */}
+          <Route exact path="/mapFound" component={MapFound}></Route>
+          {/* 城市列表 */}
+          <Route exact path="/citySelect" component={CitySelect}></Route>
+        </Router>}
+      </div>
+    );
+  }
+}
+//获取城市数据
+//重命名
+const chongmingming = (state) => {
+  //state ：管理员仓库里面的defaultState
+  return {
+    CityGPS: state.CityMap.city
+  }
 }
 //让所有页面都可以获取到 获取位置(异步action)
 const paifa = (dispatch) => {
   return {
-      paifachaifen: function (data) {
-          dispatch(yibuaction(data))
-      }
+    paifachaifen: function (data) {
+      dispatch(yibuaction(data))
+    }
   }
 }
-export default connect(null, paifa)(App);
+export default connect(chongmingming, paifa)(App);
--- src/pages/utils/baiduMap.js
+++ src/pages/utils/baiduMap.js
@@ -4,6 +4,7 @@ export const Gatweizhi = () => {
         //获取定位信息
         var myCity = new window.BMap.LocalCity();
         myCity.get(function (result) {
+            console.log(11)
             reslove(result)
         });
     })

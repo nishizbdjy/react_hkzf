daabcc865725cd703a6ec3be85f16874e4360919
lbw <2659154921@qq.com>
2020-03-25 11:43:05 +0800

    地图找房地图添加控件

--- src/pages/mapFound/index.js
+++ src/pages/mapFound/index.js
@@ -10,6 +10,10 @@ class mapFound extends Component {
     var map = new window.BMap.Map("container");    // 创建Map实例
     map.centerAndZoom(new window.BMap.Point(center.lng, center.lat), 11);  // 初始化地图,设置中心点坐标和地图级别
     //添加地图类型控件
+    map.addControl(new window.BMap.NavigationControl());    
+    map.addControl(new window.BMap.ScaleControl());    
+    map.addControl(new window.BMap.OverviewMapControl());    
+    map.addControl(new window.BMap.MapTypeControl());    
     // 设置地图显示的城市 此项是必须设置的
     map.setCurrentCity(name);
     //允许滚动

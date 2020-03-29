2df0236584cecec94183901ca49556b3a90ba33c
lbw <2659154921@qq.com>
2020-03-29 16:00:13 +0800

    通过可视化插件滑动事件触底发送请求下一页数据、筛选请求，数据的判断，及节流阀

--- src/pages/found/index.js
+++ src/pages/found/index.js
@@ -13,38 +13,76 @@ import { connect } from 'react-redux';
 import { List } from 'react-virtualized'
 class Found extends Component {
   state = {
-    //筛选条件的分页
-    params: {
-      cityId: '',//城市id
-      start: 1, //开始
-      end: 20,//结束
-    },
     list: [],//城市列表
     //筛选条件
-    screen: []
+    screen: [],
+    //总长度
+    Count: 0
   }
+  //筛选条件的分页
+  params = {
+    cityId: '',//城市id
+    start: 1, //开始
+    end: 20,//结束
+  }
+  //请求开关
+  IsLoadding = false
   async componentDidMount() {
     const { cityName } = this.props
     //获取城市id
     let id = (await axios.get('/area/info?name=' + cityName)).data.body.value
     //赋值
-    this.setState({ city: id })
+    this.params.cityId = id
     //调用获取房源列表
     this.getList()
   }
   //接收筛选组件的函数,子组件会调用
-  handleFiltrate = (data) => {
-    console.log(data)
+  handleFiltrate = (screen) => {
+    // 重置 页码 和 数组 
+    this.params.start = 1;
+    this.params.end = 20;
+    //给筛选条件赋值  、列表清空
+    this.setState({ screen, list: [] })
+    //调用筛选请求
+    this.getList()
   }
   //筛选获取列表
   getList = async () => {
-    const { params, screen } = this.state
-    const res = (await axios.get("/houses", { Params: { ...params, ...screen } })).data.body
+    const { screen } = this.state
+    const res = (await axios.get("/houses", { params: { ...this.params, ...screen } })).data.body
+    //总长度
     const { list } = res
-    //赋值
-    this.setState({ list: list })
+    //赋值 push
+    this.setState({ list: [...this.state.list, ...list], Count: res.count })
+    //开关设为空闲
+    this.IsLoadding = false
   }
-
+  //可视化插件滑动事件
+  huadong = ({ clientHeight, scrollHeight, scrollTop }) => {
+    console.clear()
+    console.log("clientHeight    " + clientHeight)
+    console.log("scrollHeight    " + scrollHeight)
+    console.log("scrollTop   " + scrollTop)
+    const { Count, list } = this.state
+    //（滚动长列表的高度-滚动区域高度）-滚动条距离底部的高度 <5触发触底加载
+    const tiaojian = scrollHeight - clientHeight - scrollTop < 5
+    //  是否还有下一页 开始条数不能大于总条数（还可以判断长度与总数）
+    const hasMore = this.params.start < Count;
+    //list长度不为零才触发事件
+    const hasLength = this.state.list.length > 0;
+    //节流阀
+    const kaiguan = this.IsLoadding == false
+    if (tiaojian && hasMore && hasLength && kaiguan) {
+      this.IsLoadding = true
+      //加载下一页
+      this.params.start += 20
+      this.params.end += 20
+      //请求
+      this.getList()
+    } else {
+      console.log('不满足条件')
+    }
+  }
   //可视化组件渲染函数
   Listhanshu = ({ style, key, index }) => {
     return <div style={style} className={foundCss.Found_List_item} key={key}><FoundList item={this.state.list[index]} />
@@ -73,15 +111,15 @@ class Found extends Component {
         {/* 房源列表展示 */}
         {/* 可视化组件 */}
         <div className={foundCss.Found_List}>
-        <List
-          width={window.screen.width}
-          height={window.screen.height - 135}
-          rowHeight={120}
-          rowCount={this.state.list.length}
-          rowHeight={128}
-          rowRenderer={this.Listhanshu}
-        />
-      </div>
+          <List
+            width={window.screen.width}
+            height={window.screen.height - 135}
+            rowCount={this.state.list.length}
+            rowHeight={128}
+            rowRenderer={this.Listhanshu}
+            onScroll={this.huadong}
+          />
+        </div>
       </div>
     );
   }
